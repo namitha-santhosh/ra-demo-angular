9dd669746e3761662e440eb6f2c1705d37642ba7
namitha-santhosh <namitha.santhosh@people10.com>
2023-09-08 00:21:52 +0530

    Signup page modified
    Validators added to signup page.

--- src/app/signup-page/signup-page.component.ts
+++ src/app/signup-page/signup-page.component.ts
@@ -1,17 +1,21 @@
+
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { Observable, Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-signup-page',
   templateUrl: './signup-page.component.html',
   styleUrls: ['./signup-page.component.css']
 })
+
+
 export class SignupPageComponent {
-  public signUpForm !: FormGroup
-  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) { }
+  public signUpForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) {}
 
   ngOnInit(): void {
     this.signUpForm = this.formBuilder.group({
@@ -19,15 +23,13 @@ export class SignupPageComponent {
       phone: ["", Validators.required],
       email: ["", [Validators.required, Validators.email]],
       password: ["", [Validators.required, Validators.minLength(5)]]
-    })
+    });
   }
 
-
   signUp() {
-    if (this.signUpForm.valid){
-    console.log(this.signUpForm.value);
-    this.http.post<any>("http://localhost:3000/signupUsersList", this.signUpForm.value)
-      .subscribe({
+    if (this.signUpForm.valid) {
+      console.log(this.signUpForm.value);
+      this.http.post<any>("http://localhost:3000/signupUsersList", this.signUpForm.value).subscribe({
         next: (res) => {
           alert('Sign Up Successful');
           this.signUpForm.reset();
@@ -37,11 +39,8 @@ export class SignupPageComponent {
           alert("Something went wrong");
         }
       });
-    }
-    else {
-      alert("Please enter all the required values.")
+    } else {
+      alert("Please enter all the required values.");
     }
   }
-  
-
 }
